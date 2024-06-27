a5d1c230eb24c6bab218ea1a4820f5f2271d29b2
Matheus Correia <matheus.correia@finderup.com.br>
2024-06-27 12:39:34 -0300

    fix: unused data

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>This data is coming from TPRC: {greeting}</h1>
+      <h1>Hello</h1>
     </main>
   );
 }
